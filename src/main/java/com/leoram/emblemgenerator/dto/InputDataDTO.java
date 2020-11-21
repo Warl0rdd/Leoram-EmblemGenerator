7be37e74a371aa922718039767ca2499e7ca6b69
Sanya <tycoon111222333@gmail.com>
2020-11-22 00:53:45 +0300

    Generating image from 3 tests

--- src/main/java/com/leoram/emblemgenerator/dto/InputDataDTO.java
+++ src/main/java/com/leoram/emblemgenerator/dto/InputDataDTO.java
@@ -1,4 +1,31 @@
 package com.leoram.emblemgenerator.dto;
 
+import lombok.*;
+
+import java.util.ArrayList;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class InputDataDTO {
+
+    public enum blazonType {
+        HORISONTAL,
+        VERTICAL,
+        QUART,
+        PLAIN
+    }
+
+    @Getter
+    @NonNull        
+    ArrayList<String> background;
+
+    @Getter
+    @NonNull
+    ArrayList<Integer> emblems;
+
+    @Getter
+    @NonNull
+    blazonType type;
+
 }
